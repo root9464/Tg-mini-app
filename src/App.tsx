6beb7da9beb966e8650463ead10e2e5551b4e2f2
iO <senpai9464@yandex.ru>
2024-06-28 21:10:45 +0500

    tg mini app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import {useViewport} from '@tma.js/sdk-react';
+import {useEffect} from 'react';
+import './App.css';
 
 export default function App() {
+  const vp = useViewport();
+  useEffect(() => {
+    console.log(vp);
+    vp?.expand();    
+  }, [vp]);
+
 
   return (
-    <>
-      <h1>Какой то текст</h1>
-    </>
+    <div>
+      <h1>Какой то текст 2</h1>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import {SDKProvider} from '@tma.js/sdk-react'
+import eruda from 'eruda'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+eruda.init()
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SDKProvider>
+      <App />
+    </SDKProvider>
   </React.StrictMode>,
 )
